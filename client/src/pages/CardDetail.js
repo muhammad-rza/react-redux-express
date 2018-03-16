f3ff8c52199b14b5d74763b5ca715d5a32e403f8
muhammad-rza <muhammadrza.dev@gmail.com>
2018-03-16 17:01:07 +0400

    route

---
+++ client/src/pages/CardDetail.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+
+import CardDetail from  '../components/Card/CardDetail';
+
+
+class App extends Component {
+	render() {
+
+		return (
+			<h1>CardDetail</h1>
+		);
+
+	}
+}
+
+export default App;
--- client/src/routes/index.js
+++ client/src/routes/index.js
@@ -3,11 +3,26 @@ import React from 'react';
 import { BrowserRouter , Route} from 'react-router-dom';
 
 import Index from  '../pages/Index';
+import CardDetail from  '../pages/CardDetail';
 
 export default ()=> (
 
 	<BrowserRouter>
-		<Route path="/" component={Index} />
+		<Route path="/" component={Index}>
+
+			<Route path='/book/:bookId' component={CardDetail} />
+
+		</Route>
+
+
 	</BrowserRouter>
 
 )
+
+
+
+// <Match pattern="/users" children={() => [
+// <Match key="1" pattern="create" component={CreateUser}/>,
+// <Match key="2" pattern="delete" component={DeleteUser}/>
+// ]}/>
+//
